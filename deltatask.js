53737659b08d11bdf16508603c5e90a743ecdac8
Sanghamitra <sanghamitrav26@gmail.com>
2018-05-17 20:15:23 +0530

    modified

--- deltatask.js
+++ deltatask.js
@@ -2,119 +2,6 @@ var menteename, dp, menteeurl, comments;
 var mentees=[];
 
 var search = document.querySelector("nav");
-function save(){
-    document.getElementById('MenteeInputForm').addEventListener('save', saveMentee);
-    function saveMentee(e) {
-        var menteename = chance.guid();
-        var dp = document.getElementById("dp").value;
-        var menteeurl = document.getElementById("menteeurl").value;
-        var comments = document.getElementById("comments").value;
-        var MenteeStatus = "Open";
-      
-        var mentee = {
-          MenteeName: menteename,
-          DisPic: dp,
-          MenteeUrl: menteeurl,
-          MenteeComments: comments,
-          status: MenteeStatus
-        }
-        
-        if (localStorage.getItem('mentees') === null) {
-          var mentees = [];
-          mentees.push(mentee);
-          localStorage.setItem('mentees', JSON.stringify(mentees));
-        } else {
-          var mentees = JSON.parse(localStorage.getItem('mentees'));
-          mentees.push(mentee);
-          localStorage.setItem('mentees', JSON.stringify(mentees));
-        }
-        
-        document.getElementById('MenteeInputForm').reset();
-       
-        add();
-        
-        e.preventDefault(); 
-      }
-
-}
-function add(){
-var menteename = JSON.parse(localStorage.getItem("menteename"));
-var dp = JSON.parse(localStorage.getItem("dp"));
-var menteeurl = JSON.parse(localStorage.getItem("menteeurl"));
-var comments = JSON.parse(localStorage.getItem("comments"));    
-for (var i = 0; i < mentees.length; i++) {
-    var menteename = mentees[i].menteename;
-    var dp = mentees[i].menteeurl;
-    var comments = mentees[i].comments;
-
-    menteenamesList.innerHTML +=   '<div class="well">'+
-                              '<h6>Mentee Name: ' + menteename + '</h6>'+'<br>'+
-                              '<h6>Display Picture: '+'<img><span class="pic">' + dp + '</span>'+'<br>'+
-                              '<h6>Profile: ' + menteeurl + '</h6>'+
-                              '<h6>Comments: ' + comments + '</h6>'+
-                              
-                              '<a href="#" class="btn btn-warning" onclick="setStatusEdit(\''+id+'\')">Edit</a> '+
-                              '<a href="#" class="btn btn-danger" onclick="deleteMentee(\''+id+'\')">Delete</a>'+
-                              '</div>';
-  }
-}
-
-function edit(){
-    function setStatusEdit (id) {
-        var menteename = JSON.parse(localStorage.getItem("menteename"));
-        var dp = JSON.parse(localStorage.getItem("dp"));
-        var menteeurl = JSON.parse(localStorage.getItem("menteeurl"));
-        var comments = JSON.parse(localStorage.getItem("comments"));
-        for(var i = 0; i < mentees.length; i++) {
-          if (mentees[i].id == id) {
-            mentees[i].status = "Edit";
-          }
-        }
-          
-        localStorage.setItem("mentees", JSON.stringify(mentees));
-        
-        add();
-      }
-
-}
-function del(){
-    function deleteMentee (id) {
-        var menteename = JSON.parse(localStorage.getItem('menteename'));
-        var dp = JSON.parse(localStorage.getItem("dp"));
-        var menteeurl = JSON.parse(localStorage.getItem("menteeurl"));
-        var comments = JSON.parse(localStorage.getItem("comments"));
-
-        
-        for(var i = 0; i < mentees.length; i++) {
-          if (mentees[i].id == id) {
-            mentees.splice(i, 1);
-          }
-        }
-        
-        localStorage.setItem("mentees", JSON.stringify(mentees));
-        
-        add();
-      }
-
-}
-
-
-
-
-
-
-var x = document.createElement("INPUT");
-x.setAttribute("type", "image");
-
-var x = document.getElementById("myImage");
-
-var yellowstar = document.getElementsByClassName("fa fa-star");
-var out = document.getElementById("rats");
-out.innerHTML = yellowstar.value;
-
-yellowstar.oninput = function star(){
-    out.innerHTML = this.value;
-}
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("rat");
@@ -133,3 +20,107 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
   }
 }
+
+var x = document.createElement("INPUT");
+x.setAttribute("type", "image");
+
+var x = document.getElementById("myImage");
+
+function save(e) {
+    var menteename = document.getElementById("menteename").value;
+    var dp = document.getElementById("dp").value;
+    var menteeurl = document.getElementById("menteeurl").value;
+    var comments = document.getElementById("comments").value;
+    var output = document.getElementById("rat").value;
+    var MenteeStatus = "Open";
+      
+    var mentee = {
+        MenteeName: menteename,
+        DisPic: dp,
+        MenteeUrl: menteeurl,
+        MenteeComments: comments,
+        Rating: output,
+        status: MenteeStatus
+        }
+        
+    if (localStorage.getItem('mentees') === null) {
+        mentees.push(mentee);
+        localStorage.setItem('mentees', JSON.stringify(mentees));
+    } else {
+        var mentees = JSON.parse(localStorage.getItem('mentees'));
+        mentees.push(mentee);
+        localStorage.setItem('mentees', JSON.stringify(mentees));
+    }
+        
+    document.getElementById('MenteeInputForm').reset();
+       
+    add();
+        
+    e.preventDefault(); 
+}
+
+function add(){
+for (var i = 0; i < mentees.length; i++) {
+    var menteename = mentees[i].menteename;
+    var dp = mentees[i].dp;
+    var menteeurl = mentees[i].menteeurl;
+    var comments = mentees[i].comments;
+    var output = mentees[i].output;
+
+    document.getElementById("menteenamesList").innerHTML += '<li>'+ '<div class="new">'+
+                              '<h6>Mentee Name: ' + menteename + '</h6>'+'<br>'+
+                              '<h6>Display Picture: '+'<img><span class="pic">' + dp + '</span>'+'<br>'+
+                              '<h6>Profile: ' + menteeurl + '</h6>'+'<br>'+
+                              '<h6>Comments: ' + comments + '</h6>'+'<br>'+
+                              '<h6>Rating: '+ output + '</h6>'+'<br>'+
+                              '<input type="image" src="close-icon.png" name="close" alt="close" width="20" height="20" formaction="#" onclick="del(\''+id+'\')">'+
+                              '<input type="image" src="edit-icon-png-24.png" name="edit" alt="edit" width="23" height="23" formaction="#" onclick="edit(\''+id+'\')">'+
+                              '</div>'+'<li>';
+  }
+}
+
+
+    function edit(id) {
+        for(var i = 0; i < mentees.length; i++) {
+          if (mentees[i].id == id) {
+            mentees[i].status = "Edit";
+            document.getElementById("MenteeInputForm").value = mentees[i];
+          } 
+        }
+          
+        localStorage.setItem("mentees", JSON.stringify(mentees));
+        
+        add();
+      }
+
+
+    function del(id) {
+        for(var i = 0; i < mentees.length; i++) {
+          if (mentees[i].id == id) {
+            mentees.splice(i, 1);
+            document.getElementById("menteenamesList")=mentees[i].remove();
+          }
+        }
+        
+        localStorage.setItem("mentees", JSON.stringify(mentees));
+        
+        add();
+      }
+
+
+
+
+
+
+
+
+
+
+var yellowstar = document.getElementsByClassName("fa fa-star");
+var out = document.getElementById("rats");
+out.innerHTML = yellowstar.value;
+
+yellowstar.oninput = function star(){
+    out.innerHTML = this.value;
+}
+
