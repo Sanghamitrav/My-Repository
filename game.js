ec74527a9cd6792927817c0ce26e6b458fd60aa3
Sanghamitra <sanghamitrav26@gmail.com>
2018-06-10 18:19:34 +0530

    modified

--- game.js
+++ game.js
@@ -40,18 +40,44 @@ function getDistance(x1, x2, y1, y2){
 
 }
 
-var v = 500;
+/*var v = 500;
 var w = 800;
 var x = 1065;
 var y = 1350;
-var z = 1650;
+var z = 1650;*/
+var v; //= Math.floor(Math.random() * 1855);
+var w; //= Math.floor(Math.random() * 1855);
+var x; //= Math.floor(Math.random() * 1855);
+var y; //= Math.floor(Math.random() * 1855);
+var z; //= Math.floor(Math.random() * 1855);
+
+
+rectArray = [
+    /*c.fillRect(v, 482, 20, 500),
+    c.fillRect(w, 0, 20, 700),
+    c.fillRect(x, 682, 20, 300),
+    c.fillRect(y, 0, 20, 700),
+    c.fillRect(z, 482, 20, 500)*/
+    500, 800, 1065, 1350, 1650
+];
 
 function rect() {
-    c.fillRect(v, 482, 20, 500);
+    /*c.fillRect(v, 482, 20, 500);
     c.fillRect(w, 0, 20, 700);
     c.fillRect(x, 682, 20, 300);
     c.fillRect(y, 0, 20, 700);
-    c.fillRect(z, 482, 20, 500);
+    c.fillRect(z, 482, 20, 500);*/
+    this.v = rectArray[Math.floor(Math.random() * rectArray.length)];
+    this.w = rectArray[Math.floor(Math.random() * rectArray.length)];
+    this.x = rectArray[Math.floor(Math.random() * rectArray.length)];
+    this.y = rectArray[Math.floor(Math.random() * rectArray.length)];
+    this.z = rectArray[Math.floor(Math.random() * rectArray.length)];
+
+    c.fillRect(this.v, 482, 20, 500);
+    c.fillRect(this.w, 0, 20, 700);
+    c.fillRect(this.x, 682, 20, 300);
+    c.fillRect(this.y, 0, 20, 700);
+    c.fillRect(this.z, 482, 20, 500);
 
     v -= 1;
     w -= 1;
@@ -60,6 +86,7 @@ function rect() {
     z -= 1;
 
 }
+
 
 
 function circle() {
@@ -86,7 +113,7 @@ function animate() {
     
 
     console.log("hiii");
-    console.log(getDistance(circle.x, circle.y, rect.x, rect.y));
+    //console.log(getDistance(circle.x, circle.y, rect.x, rect.y));
 }
 animate();
 
